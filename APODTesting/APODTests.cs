2e9eb4dc002d04adfa69e689ee2db5138d731d5a
Alexander Johnson <alexjohnson2004@gmail.com>
2025-11-09 18:16:40 +0000

    Add empty testing

---
+++ APODTesting/APODTests.cs
@@ -0,0 +1,25 @@
+﻿namespace APODTesting
+{
+    [TestClass]
+    public sealed class APODTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+
+        }
+    }
+}
---
+++ APODTesting/ConfiguratorTest.cs
@@ -0,0 +1,17 @@
+namespace APODTesting;
+
+[TestClass]
+public class ConfiguratorTest
+{
+    [TestInitialize]
+    public void Setup()
+    {
+
+    }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+
+    }
+}
